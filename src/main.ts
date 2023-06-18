6ce830e6ceac0e551a84e839829a8e912776d8cc
whoiscaio <whoiscaio@protonmail.com>
2023-06-17 21:52:13 -0300

    psql connection

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,22 @@
 import express from 'express';
+import { Client } from 'pg';
 import router from './routes';
+
+const client = new Client({
+  host: 'localhost',
+  port: 5432,
+  user: 'root',
+  password: 'root',
+  database: 'ecommerce'
+});
+
+client.connect()
+  .then(() => {
+    console.log('Successfully connected to PostgreSQL database');
+  })
+  .catch(() => {
+    console.log('Some error ocurred while trying to connect to your PostgreSQL database');
+  });
 
 const PORT = 3000;
 
